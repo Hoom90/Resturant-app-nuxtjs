c3c4cf2bf0af858940443c98a212eb10bfd8a386
M.Mahdavi <mahdavi@sahab.ir>
2024-01-17 14:15:21 +0330

    button modal components

--- components/mm-button.vue
+++ components/mm-button.vue
@@ -1,11 +1,11 @@
 <template>
-  <button :type="props.type??'button'" class="border p-2 px-10 rounded" :class="props.class">
+  <button :type="props.type??'button'" class="border p-2 px-10 rounded" :class="props.class" :disabled="props.disabled ?? false">
     <slot/>
   </button>
 </template>
 
 <script  setup>
-const props = defineProps(["type","class"])
+const props = defineProps(["type","class","disabled"])
 </script>
 
 <style>
--- components/mm-modal.vue
+++ components/mm-modal.vue
@@ -1,10 +1,10 @@
 <template>
-  <div v-if="modal" class="absolute top-0 left-0 w-full h-screen flex justify-center items-center">
+  <div v-if="modal" class="absolute top-0 left-0 w-full h-full flex justify-center items-center bg-[#00000079] z-[100]" @click="closeModal">
     <div class=" w-[500px] h-[500px] bg-white shadow rounded p-5 relative flex-row items-start justify-start">
       <button class="absolute bg-transparent border-none" @click="closeModal"><img :src="closeIcon"></button>
       <div class="text-center">header</div>
       <div class="h-[calc(100%-65px)]">
-        modal
+        <slot/>
       </div>
       <div class="absolute w-[calc(100%-40px)] flex gap-1">
         <mm-button class="text-white bg-blue-500 hover:bg-blue-600" v-if="props.confirmAction">{{ props.confirmText ?? 'confirm' }}</mm-button>
