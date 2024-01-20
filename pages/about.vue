9ca656f4ebf76ed808d1cf82cdde3f151326496e
M.Mahdavi <mahdavi@sahab.ir>
2024-01-20 11:10:45 +0330

    about contact pages and some svg

--- pages/about.vue
+++ pages/about.vue
@@ -1,10 +1,21 @@
 <script setup>
 import pageLayout from '~/layouts/pageLayout.vue';
+import footerComp from "../components/footer.vue"
 
+import hero from "@/assets/svg/about.svg"
 </script>
 
 <template>
-  <div>
-    about
+  <div class="mt-40 mb-10 mx-16 grid grid-flow-col gap-5 min-h-[600px]">
+    <div class="flex justify-center items-center gap-10">
+      <img :src="hero" alt="hero" class="w-[500px] rotate-45">
+      <div>
+        <h2 class="text-orange-500 text-[25px] my-5">About Us</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet ipsa voluptatibus voluptatum neque repellat quos nostrum laboriosam corporis omnis quas! Iusto nihil maiores deserunt dicta a quaerat expedita, aut rem.</p>
+        <p>Amet ipsa voluptatibus voluptatum neque repellat quos nostrum laboriosam corporis omnis quas! aut re.</p>
+        <p>nostrum laboriosam corporis omnis quas! Iusto nihil maiores deserunt dicta a quaerat expedita, aut rem.</p>
+      </div>
+    </div>
   </div>
+  <footerComp/>
 </template>
--- pages/contact.vue
+++ pages/contact.vue
@@ -1,10 +1,26 @@
 <script setup>
 import pageLayout from '~/layouts/pageLayout.vue';
+import footerComp from "../components/footer.vue"
+
+import hero from "@/assets/svg/salad.svg"
+import hero2 from "@/assets/svg/yogurt.svg"
+import hero3 from "@/assets/svg/vegi.svg"
 
 </script>
 
 <template>
-  <div>
-    contact
+  <div class="mt-40 mb-10 mx-16 grid grid-flow-col gap-5 min-h-[600px]">
+    <div class="flex justify-center items-center">
+      <div class="flex-row justify-center items-center text-center lg:text-start lg:max-w-[50%]">
+        <h2 class="text-orange-500 text-[25px] my-5">Contact Us</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid ducimus iusto similique voluptas, neque temporibus perspiciatis dolor odit cum. Delectus voluptatum error facere repellat, alias illum quos minus ad libero.</p>
+      </div>
+      <div class="flex justify-center items-center relative w-1/2">
+        <img :src="hero" alt="hero" class="hidden lg:block">
+        <img :src="hero2" alt="hero" class="hidden lg:block absolute top-0 -left-28">
+        <img :src="hero3" alt="hero" class="hidden lg:block absolute -bottom-10 -right-64">
+      </div>
+    </div>
   </div>
+  <footerComp/>
 </template>
